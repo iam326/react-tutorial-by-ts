d04af4d322caeac1d002086cd1bf136ab772754c
t.takahashi <takahashi.developer@gmail.com>
2020-01-05 23:47:56 +0900

    インタラクティブなコンポーネントを作る

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,25 @@ interface SquareProps {
   value: number;
 }
 
-class Square extends React.Component<SquareProps> {
+interface SquareState {
+  value: string | null;
+}
+
+class Square extends React.Component<SquareProps, SquareState> {
+  constructor(props: SquareProps) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button 
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
